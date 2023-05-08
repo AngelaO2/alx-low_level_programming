9902d5f16f14a039c29d8816995a686f8e5ef9bf
AngelaO2 <angela.nayele@outlook.com>
2023-05-08 01:19:49 +0000

    my first commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,13 @@
 
 /**
  *_abs  - check the code
- *@r:an integer input 
- *Return: Always 0. 
+ *@r:an integer input
+ *Return: Always 0.
  */
 
 int _abs(int r)
 {
 	if (r < 0)
 		r = r * -1;
-	return(r);
+	return (r);
 }
